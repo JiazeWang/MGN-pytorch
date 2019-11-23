9df72e8bfcb1c20c425f68352a9cef76c849b811
JiazeWang <jiazewang@csu.edu.cn>
2019-11-24 00:45:33 +0800

    1

--- model/mgn.py
+++ model/mgn.py
@@ -3,17 +3,56 @@ import copy
 import torch
 from torch import nn
 import torch.nn.functional as F
-
+import datetime
+from option import args
 from torchvision.models.resnet import resnet50, Bottleneck
+from non_local_2D import Nonlocal
+class ConvBlock(nn.Module):
+    """Basic convolutional block:
+    convolution + batch normalization + relu.
+
+    Args (following http://pytorch.org/docs/master/nn.html#torch.nn.Conv2d):
+    - in_c (int): number of input channels.
+    - out_c (int): number of output channels.
+    - k (int or tuple): kernel size.
+    - s (int or tuple): stride.
+    - p (int or tuple): padding.
+    """
+    def __init__(self, in_c, out_c, k, s=1, p=0):
+        super(ConvBlock, self).__init__()
+        self.conv = nn.Conv2d(in_c, out_c, k, stride=s, padding=p)
+        self.bn = nn.BatchNorm2d(out_c)
+
+    def forward(self, x):
+        return F.relu(self.bn(self.conv(x)))
 
 def make_model(args):
     return MGN(args)
+
+class ChannelAttn(nn.Module):
+
+    def __init__(self, in_channels=256, reduction_rate=16):
+        super(ChannelAttn, self).__init__()
+        assert in_channels%reduction_rate == 0
+        self.conv1 = ConvBlock(in_channels, in_channels // reduction_rate, 1)
+        self.conv2 = ConvBlock(in_channels // reduction_rate, in_channels, 1)
+
+    def forward(self, input):
+        # squeeze operation (global average pooling)
+        x = input
+        #x = F.avg_pool2d(x, x.size()[2:])
+        # excitation operation (2 conv layers)
+        x = self.conv1(x)
+        x = self.conv2(x)
+        #return x*input + input
+        return x * input
+
 
 class MGN(nn.Module):
     def __init__(self, args):
         super(MGN, self).__init__()
         num_classes = args.num_classes
-
+        #num_classes = 100
         resnet = resnet50(pretrained=True)
 
         self.backone = nn.Sequential(
@@ -25,6 +64,10 @@ class MGN(nn.Module):
             resnet.layer2,
             resnet.layer3[0],
         )
+        self.pixel_attention = Nonlocal(dim = 1024, dim_inner=512)
+        self.pixel_attention1 = Nonlocal(dim = 2048, dim_inner=512)
+        self.pixel_attention2 = Nonlocal(dim = 2048, dim_inner=512)
+        self.pixel_attention3 = Nonlocal(dim = 2048, dim_inner=512)
 
         res_conv4 = nn.Sequential(*resnet.layer3[1:])
 
@@ -75,7 +118,7 @@ class MGN(nn.Module):
         self.fc_id_256_2_0 = nn.Linear(args.feats, num_classes)
         self.fc_id_256_2_1 = nn.Linear(args.feats, num_classes)
         self.fc_id_256_2_2 = nn.Linear(args.feats, num_classes)
-
+        self.channel_attention = ChannelAttn()
 
         self._init_fc(self.fc_id_2048_0)
         self._init_fc(self.fc_id_2048_1)
@@ -91,7 +134,6 @@ class MGN(nn.Module):
     def _init_reduction(reduction):
         # conv
         nn.init.kaiming_normal_(reduction[0].weight, mode='fan_in')
-        #nn.init.constant_(reduction[0].bias, 0.)
 
         # bn
         nn.init.normal_(reduction[1].weight, mean=1., std=0.02)
@@ -106,10 +148,14 @@ class MGN(nn.Module):
     def forward(self, x):
 
         x = self.backone(x)
-        #print(x.shape)
+        x = self.pixel_attention(x)
+
         p1 = self.p1(x)
+        p1 = self.pixel_attention1(p1)
         p2 = self.p2(x)
+        p2 = self.pixel_attention2(p2)
         p3 = self.p3(x)
+        p3 = self.pixel_attention3(p3)
 
         zg_p1 = self.maxpool_zg_p1(p1)
         zg_p2 = self.maxpool_zg_p2(p2)
@@ -124,14 +170,23 @@ class MGN(nn.Module):
         z1_p3 = zp3[:, :, 1:2, :]
         z2_p3 = zp3[:, :, 2:3, :]
 
-        fg_p1 = self.reduction_0(zg_p1).squeeze(dim=3).squeeze(dim=2)
-        fg_p2 = self.reduction_1(zg_p2).squeeze(dim=3).squeeze(dim=2)
-        fg_p3 = self.reduction_2(zg_p3).squeeze(dim=3).squeeze(dim=2)
-        f0_p2 = self.reduction_3(z0_p2).squeeze(dim=3).squeeze(dim=2)
-        f1_p2 = self.reduction_4(z1_p2).squeeze(dim=3).squeeze(dim=2)
-        f0_p3 = self.reduction_5(z0_p3).squeeze(dim=3).squeeze(dim=2)
-        f1_p3 = self.reduction_6(z1_p3).squeeze(dim=3).squeeze(dim=2)
-        f2_p3 = self.reduction_7(z2_p3).squeeze(dim=3).squeeze(dim=2)
+        fg_p1 = self.reduction_0(zg_p1)#.squeeze(dim=3).squeeze(dim=2)
+        fg_p2 = self.reduction_1(zg_p2)#.squeeze(dim=3).squeeze(dim=2)
+        fg_p3 = self.reduction_2(zg_p3)#.squeeze(dim=3).squeeze(dim=2)
+        f0_p2 = self.reduction_3(z0_p2)#.squeeze(dim=3).squeeze(dim=2)
+        f1_p2 = self.reduction_4(z1_p2)#.squeeze(dim=3).squeeze(dim=2)
+        f0_p3 = self.reduction_5(z0_p3)#.squeeze(dim=3).squeeze(dim=2)
+        f1_p3 = self.reduction_6(z1_p3)#.squeeze(dim=3).squeeze(dim=2)
+        f2_p3 = self.reduction_7(z2_p3)#.squeeze(dim=3).squeeze(dim=2)
+
+        fg_p1 = self.channel_attention(fg_p1).squeeze(dim=3).squeeze(dim=2)
+        fg_p2 = self.channel_attention(fg_p2).squeeze(dim=3).squeeze(dim=2)
+        fg_p3 = self.channel_attention(fg_p3).squeeze(dim=3).squeeze(dim=2)
+        f0_p2 = self.channel_attention(f0_p2).squeeze(dim=3).squeeze(dim=2)
+        f1_p2 = self.channel_attention(f1_p2).squeeze(dim=3).squeeze(dim=2)
+        f0_p3 = self.channel_attention(f0_p3).squeeze(dim=3).squeeze(dim=2)
+        f1_p3 = self.channel_attention(f1_p3).squeeze(dim=3).squeeze(dim=2)
+        f2_p3 = self.channel_attention(f2_p3).squeeze(dim=3).squeeze(dim=2)
 
 
         l_p1 = self.fc_id_2048_0(fg_p1)
@@ -147,3 +202,17 @@ class MGN(nn.Module):
         predict = torch.cat([fg_p1, fg_p2, fg_p3, f0_p2, f1_p2, f0_p3, f1_p3, f2_p3], dim=1)
 
         return predict, fg_p1, fg_p2, fg_p3, l_p1, l_p2, l_p3, l0_p2, l1_p2, l0_p3, l1_p3, l2_p3
+
+if __name__ == "__main__":
+    input_tensor = torch.rand(8, 3, 384, 128)
+    model = MGN(args)
+    print(model)
+    starttime = datetime.datetime.now()
+    output = model(input_tensor)
+    endtime = datetime.datetime.now()
+    print(output[0].size())
+    print((endtime - starttime).seconds)
+    num_params=0
+    for param in model.parameters():
+        num_params += param.reshape((-1, 1)).shape[0]
+    print("Model Size is {:.3f}M".format(num_params/1000000))
